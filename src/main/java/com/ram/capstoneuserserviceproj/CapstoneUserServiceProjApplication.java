eeacb2734ab7e5787447f98511808609fb23b805
unknown <raghuram6857@gmail.com>
2024-02-23 17:25:21 +0530

    User Service Creation

---
+++ src/main/java/com/ram/capstoneuserserviceproj/CapstoneUserServiceProjApplication.java
@@ -0,0 +1,13 @@
+package com.ram.capstoneuserserviceproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneUserServiceProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneUserServiceProjApplication.class, args);
+    }
+
+}
